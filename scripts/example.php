310c59e96066ff1847f3388b7ca232c9f6474b6a
Tybio <tybio@mux.net>
2013-01-13 17:42:17 -0500

    Now a working example

--- scripts/example.php
+++ scripts/example.php
@@ -5,22 +5,31 @@ echo "Testing Category functions\n";
 
 $stat = new Stats();
 
-$list = $stat->getList('count');
-$ret = $stat->getStats('count', 'ALL');
+$ret = $stat->getStats('ppq', 'count');
 
-$output = buildTable($ret);
+drawSmallTable($ret);
 
-function buildTable($array) {
-	$count = count($array);
-	echo "Building table for $count items\n";
-	$width = (int) (80/$count);
-	echo "Using $width width\n";
-	$format = '|%-10.9s|%-15.15s|%-13.13s|%-13.13s|%-13.13s|';
+function drawSmallTable($array) {
+        $i=0;
+	$line = '+'.str_repeat('-', '35').'+';
+	echo "$line\n";
+	foreach ($array as $key => $value) {
+		$number = number_format($value);
+		printf("| %-15s |",   $key );
+		printf(" %-15s |",  $number);
+		echo "\n";
+	}
+	echo "$line\n";
+}
 
-	foreach ( $array as $key => $i ) {
-		$number = number_format($i);
-		$width = 
-		$out[] = '+'.str_repeat('-',68).'+';
+function drawBigTable($array) {
+	$i=0;
+	foreach( $array as $items) {
+		foreach ($items as $key => $value) {
+			if ($i++==0) { printf("[%-10s]|",   $key ); }
+			printf("[%-10s]|",   $value);
+		}
+		echo "\n"; 
 	}
 }
 ?>
