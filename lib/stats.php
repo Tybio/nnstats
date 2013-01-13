e74da3a45fa8872c64543f36f98078424b2448d3
Tybio <tybio@mux.net>
2013-01-13 15:14:16 -0500

    Total Rework

--- lib/stats.php
+++ lib/stats.php
@@ -23,46 +23,34 @@ require_once(WWW_DIR."/lib/category.php");
 
 class Stats {
 
-	public function __construct() {
+	private function getSQL() {
+		// Postprocessing Queue queries
 		// SQL queries based on categories
-		define('C_MOVIE', 'SELECT COUNT(id) FROM releases USE INDEX (ix_releases_categoryID) WHERE imdbID IS NULL AND categoryID IN ( SELECT ID FROM category WHERE parentID = '.Category::CAT_PARENT_MOVIE.')');
-		define('C_MUSIC', 'SELECT COUNT(id) FROM releases USE INDEX (ix_releases_categoryID) WHERE musicinfoID IS NULL AND categoryID IN ( SELECT ID FROM category WHERE parentID = '.Category::CAT_PARENT_MUSIC.")");
-		define('C_ANIME', 'SELECT COUNT(id) FROM releases WHERE anidbID IS NULL AND categoryID IN ( SELECT ID FROM category WHERE categoryID = '.Category::CAT_TV_ANIME.")");
-		define('C_TVRAGE', 'SELECT COUNT(id) FROM releases WHERE rageID = -1 AND categoryID in ( select ID from category WHERE parentID = '.Category::CAT_PARENT_TV.")");
-		define('C_TVDB', 'SELECT COUNT(id) FROM releases WHERE episodeinfoID IS NULL AND categoryID IN ( SELECT ID FROM category WHERE parentID = '.Category::CAT_PARENT_TV.")");
-		define('C_GAME', 'SELECT COUNT(id) FROM releases USE INDEX (ix_releases_categoryID) WHERE consoleinfoID IS NULL AND categoryID IN ( SELECT ID FROM category WHERE parentID = '.Category::CAT_PARENT_GAME.")");
+		$NNQ['ppq']['count']['movie'] = 'SELECT COUNT(id) FROM releases USE INDEX (ix_releases_categoryID) WHERE imdbID IS NULL AND categoryID IN ( SELECT ID FROM category WHERE parentID = '.Category::CAT_PARENT_MOVIE.')';
+		$NNQ['ppq']['count']['music'] = 'SELECT COUNT(id) FROM releases USE INDEX (ix_releases_categoryID) WHERE musicinfoID IS NULL AND categoryID IN ( SELECT ID FROM category WHERE parentID = '.Category::CAT_PARENT_MUSIC.")";
+		$NNQ['ppq']['count']['anime'] = 'SELECT COUNT(id) FROM releases WHERE anidbID IS NULL AND categoryID IN ( SELECT ID FROM category WHERE categoryID = '.Category::CAT_TV_ANIME.")";
+		$NNQ['ppq']['count']['tvrage'] = 'SELECT COUNT(id) FROM releases WHERE rageID = -1 AND categoryID in ( select ID from category WHERE parentID = '.Category::CAT_PARENT_TV.")";
+		$NNQ['ppq']['count']['tvdb'] = 'SELECT COUNT(id) FROM releases WHERE episodeinfoID IS NULL AND categoryID IN ( SELECT ID FROM category WHERE parentID = '.Category::CAT_PARENT_TV.")";
+		$NNQ['ppq']['count']['game'] = 'SELECT COUNT(id) FROM releases USE INDEX (ix_releases_categoryID) WHERE consoleinfoID IS NULL AND categoryID IN ( SELECT ID FROM category WHERE parentID = '.Category::CAT_PARENT_GAME.")";
 		// SQL queries not based on categories
-		define('C_NFO', 'SELECT COUNT(id) FROM `releasenfo` WHERE `nfo` IS NULL AND `attempts` <= 3');
-		define('C_PART', 'SELECT COUNT(id) FROM `parts`');
-		define('C_BIN', 'SELECT COUNT(id) FROM `binaries`');
+		$NNQ['ppq']['count']['movie'] = 'SELECT COUNT(id) FROM `releasenfo` WHERE `nfo` IS NULL AND `attempts` <= 3';
+
+		// Table Size
+		$NNQ['tablerows']['count']['movie'] = 'SELECT COUNT(id) FROM `releases`';
+		$NNQ['tablerows']['count']['movie'] = 'SELECT COUNT(id) FROM `parts`';
+		$NNQ['tablerows']['count']['movie'] = 'SELECT COUNT(id) FROM `binaries`';
 	}
 
-	public function getStats($type, $id = 'ALL' ) {
+	public function getStats($type, $data, $id = 'ALL' ) {
 		if ( $id === 'ALL') { 
-			return $this->getType($type);
+			return $this->getType($type, $data);
 		} else {
-			return $this->getID($type, $id);
+			return $this->getID($type, $data, $id);
 		}
 	}
 
-	public function getList($type) {
-		$prefix = $this->getPrefix($type);
-		// Returns a list of all valid IDs
-		$list = get_defined_constants(true);
-		foreach ($list['user'] as $key=>$value) {
-			$match = '/^'.$prefix.'/';
-			if ( preg_match($match, $key) ) {
-				$trim = str_replace($prefix, '', $key);
-   	 	    	$dump[$trim] = $value; 
-       		}
-        }
-    	return $dump;
-	}
-
-	private function getID($type, $id) {
-		$prefix = $this->getPrefix($type);
-		if ( constant($prefix.$id) ) {
-			$sql = constant($prefix."$id");
+	private function getID($type, $data, $id) {
+			$sql = $NNQ[$type][$data][$id];
 			$db = new DB();
 			$dbo = $db->query($sql);
 			return $dbo['0']['COUNT(id)'];
@@ -71,21 +59,14 @@ class Stats {
 		}
 	}
 
-	private function getType($type) {
-		$prefix = $this->getPrefix($type);
-		foreach ( $this->getList($type) as $k => $i ) {
-			$sql = constant($prefix.$k);
-			$db = new DB();
-			$dbo = $db->query($sql);
-			$dump[$k] = $dbo;
+	private function getType($type, $data) {
+		$list = $NNQ[$type][$data];
+		$db = new DB();
+		foreach ( $list as $key => $i) {
+			$dbo = $db->query($i);
 		}
 		$out = $this->cleanArray($dump);
 		return $out;
-	}
-
-	private function getPrefix($type) {
-		if ( $type === 'count' ) { return 'C_'; }
-		if ( $type === 'delta' ) { return 'D_'; }
 	}
 
 	private function cleanArray($array) {
